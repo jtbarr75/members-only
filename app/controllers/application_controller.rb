0daf1f2740f313a09824b036d422c72c67410e8f
Jason Barr <Barr75@gmail.com>
2020-04-08 12:07:07 -0700

    create users model and controller with authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,31 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+
+  #logs in the given user
+  def log_in(user)
+    user.remember
+    user.save
+    cookies.permanent[:remember_token] = user.remember_token
+    current_user = user
+  end
+
+  #returns the current user
+  def current_user
+    if (remember_token = cookies[:remember_token])
+      @current_user ||= User.find_by(remember_digest: User.digest(remember_token))
+    end
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def log_out
+    cookies.delete(:remember_token)
+    current_user = nil
+  end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,49 @@
+class User < ApplicationRecord
+  attr_accessor :remember_token
+  before_save :downcase_email
+  before_create :remember
+
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
+  #Returns the hash digest of the given string
+  def self.digest(string)
+    Digest::SHA1.hexdigest(string)
+  end
+
+  #returns a random token
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  #remembers a user in the databse for use in persistent sessions
+  def remember
+    self.remember_token = User.new_token
+    self.remember_digest = User.digest(remember_token)
+  end
+
+  #returns true if the given token matches the digest
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    digest == User.digest(token)
+  end
+
+  #forgets a user
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  private
+
+    #converts the email to all lower case
+    def downcase_email
+      email.downcase!
+    end
+end
